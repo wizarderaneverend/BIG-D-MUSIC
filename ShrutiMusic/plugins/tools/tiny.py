f68531dd424f2d28ca5819eab62fa7e0dd3f13c1
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-09-09 18:20:29 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/tools/tiny.py
+++ ShrutiMusic/plugins/tools/tiny.py
@@ -2,7 +2,7 @@ import os
 import cv2
 from PIL import Image
 from pyrogram import Client, filters
-from ShritiMusic import app
+from ShrutiMusic import app
 
 
 @app.on_message(filters.command("tiny"))
